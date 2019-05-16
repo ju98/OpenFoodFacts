9eeba5cc2b6ccb0b3d3f003eed88f2c37571f0e7
Juliette <juliette.1024@gmail.com>
2019-05-16 11:47:41 +0200

    Petite modif

--- main.py
+++ main.py
@@ -7,10 +7,21 @@ Created on Thu May 16 10:40:11 2019
 
 from off import OFF
 import pandas
- 
-df = pandas.read_csv('produit_pour_le_client_detail.csv', sep = ";", nrows = 100)
-OpenFoodFacts = OFF(df)
-allergens=input("Bonjour , veuillez nous indiquer les ingredients auxquels vous etes allergique (separes par une virgule s'il vous plait) afin de vous proposer des produits plus adapté : ")
-allergens = allergens.split(",")
-print("voici tous nos produits que vous pouvez consommer : ")
-print(OpenFoodFacts.productsOk(allergens))
+
+
+if __name__ == "__main__" : 
+    
+    df = pandas.read_csv('produit_pour_le_client_detail.csv', sep = ";", nrows = 100)
+    OpenFoodFacts = OFF(df)
+    
+#    print("Exemple d'allergenes :")
+#    
+#    al = OpenFoodFacts.getAllergenes()
+#    print(al)
+
+    
+    allergens = input("Bonjour , veuillez nous indiquer les ingredients auxquels vous etes allergique (separes par une virgule s'il vous plait) afin de vous proposer des produits plus adapté : ")
+    allergens = allergens.split(",")
+    
+    print("\n\nvoici tous nos produits que vous pouvez consommer : \n")
+    print(OpenFoodFacts.productsOk(allergens))
